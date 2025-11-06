029b5ac6dedcd6bc0cbce0b9f290e6b112dac4e5
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-06 17:38:01 +0700

    menambahkan enum Navigasi

---
+++ app/src/main/java/com/example/praktikum5/Navigasi.kt
@@ -0,0 +1,13 @@
+package com.example.praktikum5
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.compose.rememberNavController
+import java.lang.reflect.Modifier
+
+enum class Navigasi {
+    Formulirku,
+    Detail
+}
