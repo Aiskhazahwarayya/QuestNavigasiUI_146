ae6712729b129f82222adb6f1fc8660204d7602b
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-06 17:03:21 +0700

    menambahkan input nama lengkap

--- app/src/main/java/com/example/praktikum5/ui/theme/view/Formulir.kt
+++ app/src/main/java/com/example/praktikum5/ui/theme/view/Formulir.kt
@@ -1,10 +1,17 @@
 package com.example.praktikum5.ui.theme.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.praktikum5.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -26,7 +33,21 @@ fun FormIsian(
                 )
             )
         }
-    ) {
-
+    ) { isiRuang ->
+        Column(
+            modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(250.dp),
+                label = { Text(text = "Nama Lengkap") },
+                onValueChange = {},
+            )
+        }
     }
 }
