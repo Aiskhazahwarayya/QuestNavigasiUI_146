dec323c65a17f596cdd7d1658c886fb103242861
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-06 17:48:05 +0700

    tambahkan rute composable

--- app/src/main/java/com/example/praktikum5/Navigasi.kt
+++ app/src/main/java/com/example/praktikum5/Navigasi.kt
@@ -3,6 +3,7 @@ package com.example.praktikum5
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import androidx.navigation.compose.composable
 import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -23,6 +24,12 @@ fun DataApp(
             startDestination = Navigasi.Formulirku.name,
 
             modifier = Modifier.padding(paddingValues = isiRuang)
-        ){}
+        ){
+            composable(route = Navigasi.Formulirku.name){
+
+            }
+
+
+        }
     }
 }
