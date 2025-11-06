b537dd06140395cfca0e9a266955dd0897d1f340
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-06 17:15:34 +0700

    menambahkan layout form jenis kelamin, alamat, dan tombol submit

--- app/src/main/java/com/example/praktikum5/ui/theme/view/Formulir.kt
+++ app/src/main/java/com/example/praktikum5/ui/theme/view/Formulir.kt
@@ -2,9 +2,14 @@ package com.example.praktikum5.ui.theme.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.*
+import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -48,6 +53,43 @@ fun FormIsian(
                 label = { Text(text = "Nama Lengkap") },
                 onValueChange = {},
             )
+        }
+        HorizontalDivider(modifier = Modifier
+            .padding(20.dp)
+            .width(250.dp),thickness = Thickness,color = Color.Red
+        )
+        Row{
+            jenisK.forEach { item ->
+                Row(verticalAlignment = Alignment.CenterVertically){
+                    RadioButton(
+                        selected = false,
+                        onClick = { item }
+                    )
+                    Text(text = item)
+                }
+            }
+        }
+        HorizontalDivider(modifier = Modifier
+            .padding(20.dp)
+            .width(250.dp),
+            thickness = 1.dp,
+            color = Color.Red
+        )
+        OutlinedTextField(
+            value = "",
+            singleLine = true,
+            modifier = Modifier
+                .width(width = 250.dp),
+            label = {Text(text = "Alamat")},
+            onValueChange = {},
+
+            )
+        Spacer(modifier = Modifier.height(30.dp))
+        Button(
+            modifier = Modifier.fillMaxWidth(fraction = 1f),
+            onClick = onSubmitBtnClick
+        ){
+            Text(text = stringResource(id = R.string.submit))
         }
     }
 }
